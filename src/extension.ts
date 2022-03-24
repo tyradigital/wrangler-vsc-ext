e5a4cf45d8d2c02020c0e78d77f596187cdf398b
F1sxher <72705185+F1sxher@users.noreply.github.com>
2022-03-24 16:48:30 -0400

    feat(extension): validation things and new commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import Logger, { LogLevel } from "./utils/logger";
 
 // COMMAND IMPORTS
 import { execute as tailExec } from "./commands/tail";
@@ -9,6 +8,13 @@ import { execute as enableExec } from "./commands/enable";
 import { execute as disableExec } from "./commands/disable";
 import { execute as setCompVerExec } from "./commands/setcompver";
 import { execute as generateExec } from "./commands/generate";
+import { execute as initExec } from "./commands/init";
+
+// OTHER IMPORTS
+import Logger, { LogLevel } from "./utils/logger";
+import { getConfig } from "./utils/config";
+import { WranglerVersion } from "./utils/enums";
+import { getTerminal } from "./utils/terminal";
 
 export const outChannel = vscode.window.createOutputChannel("Wrangler Ext");
 
@@ -18,6 +24,32 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   Logger.log(LogLevel.info, "Wrangler Extension is online!");
+
+  const config = getConfig();
+
+  const validate = (func: Function, ...args: any[]) => {
+    if (config.get("enabled") === false) {
+      return vscode.window.showWarningMessage(
+        "The Wrangler Extension is disabled for this workspace\n \nEnable it using the enable command"
+      );
+    } else {
+      let compver: WranglerVersion = 1;
+
+      switch (config.get("compver") as WranglerVersion) {
+        case 1:
+          compver = 1;
+          break;
+        case 2:
+          compver = 2;
+          break;
+        default:
+          compver = 1;
+          break;
+      }
+
+      return func(compver, ...args);
+    }
+  };
 
   const enableCommand = vscode.commands.registerCommand(
     "wrangler.enable",
@@ -36,12 +68,17 @@ export function activate(context: vscode.ExtensionContext) {
 
   const tailCommand = vscode.commands.registerCommand(
     "wrangler.tail",
-    tailExec
+    (...args: any[]) => validate(tailExec, ...args)
   );
 
   const generateCommand = vscode.commands.registerCommand(
     "wrangler.generate",
-    generateExec
+    (...args: any[]) => validate(generateExec, ...args)
+  );
+
+  const initCommand = vscode.commands.registerCommand(
+    "wrangler.init",
+    initExec
   );
 
   context.subscriptions.push(
@@ -49,7 +86,8 @@ export function activate(context: vscode.ExtensionContext) {
     disableCommand,
     setCompVerCommand,
     tailCommand,
-    generateCommand
+    generateCommand,
+    initCommand
   );
 }
 
